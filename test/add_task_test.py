28f6c4616f6b5975327c110b397104a4c0899053
Helen Virtanen <helenvirtanen28@gmail.com>
2024-07-05 20:33:48 +0300

    fix tests

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -2,9 +2,9 @@ import requests
 
 
 def test_add():
-    body = {"title":"generated","completed":False}
+    body = {"title":"новая задача","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     response_body = response.json()
     
-    assert response.status_code == 201
-    assert response_body['completed'] == False
+    assert response.status_code == 200
+    assert response_body['completed'] == None
