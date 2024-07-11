8d11f2bd440e97c64b3e96185c001d04170a9094
Helen Virtanen <helenvirtanen28@gmail.com>
2024-07-11 16:34:28 +0300

    additions to code

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -8,3 +8,36 @@ def test_add():
     
     assert response.status_code == 200
     assert response_body['completed'] == None
+
+
+
+
+
+
+
+
+
+
+#def test_add_task_only_title():
+#    body = {"title":"only title"}
+#    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+#    response_body = response.json()
+#    
+#    assert response.status_code == 200
+#    assert response_body['title'] == 'only title'
+#
+#def test_add_task_without_title():
+#    body = {"title":""}
+#    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+#    response_body = response.json()
+#    
+#    assert response.status_code == 200
+#    assert response_body['title'] == ""
+#
+#def test_add_task_completed():
+#    body = {"completed":True}
+#    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+#    response_body = response.json()
+#    
+#    assert response.status_code == 404
+#    assert response_body['completed'] == True
--- test/edit_task_test.py
+++ test/edit_task_test.py
@@ -12,4 +12,48 @@ def test_edit():
     
     response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
     assert response.status_code == 200
-    assert response.json()['title']=="generated-1"
+    assert response.json()['title'] == "generated-1"
+
+
+
+
+
+#def test_edit_title():
+#    body = {"title":"first task","completed":False}
+#    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+#    id = response.json()["id"]
+#    
+#    body = {"title":"first task edited"}
+#    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
+#    assert response.status_code == 200
+#    
+#    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+#    assert response.status_code == 200
+#    assert response.json()['title'] == "first task edited"
+#
+#def test_edit_completed():
+#    body = {"title":"second task","completed":False}
+#    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+#    id = response.json()["id"]
+#    
+#    body = {"completed":True}
+#    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
+#    assert response.status_code == 200
+#    
+#    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+#    assert response.status_code == 200
+#    assert response.json()['completed'] == True
+#
+#def test_edit_both():
+#    body = {"title":"third task","completed":False}
+#    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+#    id = response.json()["id"]
+#    
+#    body = {"title":"third task edited", "completed":True}
+#    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
+#    assert response.status_code == 200
+#    
+#    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+#    assert response.status_code == 200
+#    assert response.json()['completed'] == True
+#    assert response.json()['title'] == "third task edited"
