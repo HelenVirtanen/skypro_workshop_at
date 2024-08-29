eb8062e083ae6305c4c0669501442fde22773a5f
Helen Virtanen <helenvirtanen28@gmail.com>
2024-08-29 18:00:10 +0300

    edited edit_task

--- test/edit_task_test.py
+++ test/edit_task_test.py
@@ -2,14 +2,14 @@ import requests
 
 
 def test_edit():
-    body = {"title":"generated","completed":False}
+    body = {"title":"Посмотреть фильм","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     id = response.json()["id"]
     
-    body = {"title":"generated-1"}
+    body = {"title":"Пойти в парк"}
     response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
     assert response.status_code == 200
     
     response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
     assert response.status_code == 200
-    assert response.json()['title']=="generated-1"
+    assert response.json()['title']=="Пойти в парк"
