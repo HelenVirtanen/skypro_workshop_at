7e8608e0fd35c4d813b3a330e8d88a1df088c517
HelenVirtanen <124844232+HelenVirtanen@users.noreply.github.com>
2024-10-03 18:16:19 +0300

    Update delete_task_test.py

--- test/delete_task_test.py
+++ test/delete_task_test.py
@@ -2,7 +2,7 @@ import requests
 
 
 def test_delete():
-    body = {"title":"generated","completed":False}
+    body = {"title":"Лечь спать пораньше","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     id = response.json()["id"]
     
