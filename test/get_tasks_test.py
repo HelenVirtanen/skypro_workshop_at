7d8372b02ef01f090f061c1629e3765494364ca4
Helen Virtanen <helenvirtanen28@gmail.com>
2024-10-03 20:32:27 +0300

    fixed

--- test/get_tasks_test.py
+++ test/get_tasks_test.py
@@ -1,19 +1,8 @@
 import requests
 
 
-#def get_tasks_test():
-#    response = requests.get("https://todo-app-sky.herokuapp.com/")
-#    assert response.status_code == 200
-#    assert type(response.json()) is list
-
-def get_list_tasks_test():
-    
-    body = {"title":"Сходить на воркшоп","completed":False}
-    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
-    len_response_body = len(response.json())
-
+def get_tasks_test():
     response = requests.get("https://todo-app-sky.herokuapp.com/")
-
     assert response.status_code == 200
     assert type(response.json()) is list
-    assert len_response_body == 10
+
