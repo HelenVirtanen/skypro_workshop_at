9d4d92aa4e3f1526eba387471741a43546124d18
HelenVirtanen <124844232+HelenVirtanen@users.noreply.github.com>
2024-10-03 18:44:20 +0300

    Update add_task_test.py

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -8,3 +8,11 @@ def test_add():
     
     assert response.status_code == 200
     assert response_body['completed'] == False
+
+def test_add_2():
+    body = {"title":"Сделать кофе","completed":False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    response_body = response.json()
+    
+    assert response.status_code == 400
+    assert response_body['completed'] == False
