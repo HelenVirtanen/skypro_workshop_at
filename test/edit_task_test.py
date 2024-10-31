2847fdb6bc10ce27d630f9e805cb711ae6212b37
NataliaArganashvili <nataly.arganashvili@gmail.com>
2024-10-31 21:21:03 +0400

    Update edit_task_test.py

--- test/edit_task_test.py
+++ test/edit_task_test.py
@@ -13,3 +13,20 @@ def test_edit():
     response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
     assert response.status_code == 200
     assert response.json()['title']=="Посмотреть сериал дома"
+
+
+import requests
+
+
+def test_edit():
+    body = {"title":"Пойти в кино","completed":False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    id = response.json()["id"]
+    
+    body = {"completed":True}
+    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
+    assert response.status_code == 200
+    
+    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+    assert response.status_code == 200
+    assert response.json()['completed']==True
