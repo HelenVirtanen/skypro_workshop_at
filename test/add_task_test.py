dc61888528fedb04eb53ec44839bc8fc0fc7eff6
Helen Virtanen <helenvirtanen28@gmail.com>
2024-11-14 20:22:34 +0300

    fixed test

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -1,15 +1,14 @@
 import requests
 
 
-def test_add():
+def add_task_test():
     body = {"title":"Сходить на воркшоп","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
-    response_body = response.json()
-    
+    response.json()
     assert response.status_code == 200
-    assert response_body['completed'] == None
+    assert response['completed'] == None
 
-    def test_add_2():
+def test_add_2():
     body = {"title":"Сходить на воркшоп","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     response_body = response.json()
